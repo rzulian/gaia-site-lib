1c2be0f9e4b9e4b0f4246c9b5e87e7c79cc1a501
coyotte508 <coyotte508@gmail.com>
2020-04-18 12:11:37 +0200

    First commit

---
+++ game.d.ts
@@ -0,0 +1,52 @@
+export interface IAbstractGame<T= string, Game = any, GameOptions = any> {
+  /** Ids of the players in the website */
+  players: Array<{_id: T, remainingTime: number}>;
+  creator: T;
+  currentPlayer: T;
+
+  nextMoveDeadline: Date;
+
+  /** Game data */
+  data: Game;
+
+  options: {
+    setup: {
+      seed: string;
+      nbPlayers: number;
+      randomPlayerOrder: boolean;
+    },
+    timing: {
+      timePerGame: number;
+      timePerMove: number;
+      /* UTC-based time of play, by default all day, during which the timer is active, in seconds */
+      timer: {
+        // eg 3600 = start at 1 am
+        start: number;
+        // eg 3600*23 = end at 11 pm
+        end: number;
+      }
+    },
+    meta: {
+      unlisted: boolean;
+    }
+  };
+
+  game: {
+    name: string, // e.g. "gaia-project"
+    version: string; // e.g. "1.0.0"
+    expansions: string[], // e.g. ["spaceships"]
+
+    options: GameOptions
+  };
+
+  active: boolean;
+  open: boolean;
+
+  lastMove: Date;
+  updatedAt: Date;
+  createdAt: Date;
+}
+
+export interface IGame extends IAbstractGame {
+  _id: string;
+}
---
+++ user.d.ts
@@ -0,0 +1,48 @@
+export interface IAbstractUser {
+  account: {
+    username: string,
+    email: string,
+    password: string
+  };
+  settings: {
+    mailing: {
+      newsletter: boolean,
+      game: {
+        /** Delay before sending a mail notification, in seconds */
+        delay: number,
+        /** Are email notifications enabled? */
+        activated: boolean
+      } 
+    },
+    game: {
+      noFactionFill: boolean,
+      /** Are sound notifications enabled? */
+      soundNotification: boolean
+    }
+  };
+  security: {
+    lastIp: string,
+    lastLogin: {
+      ip: string,
+      date: Date
+    },
+    confirmed: boolean,
+    confirmKey: string,
+    reset: {
+      key: string,
+      issued: Date
+    }
+  };
+  meta: {
+    nextGameNotification: Date,
+    lastGameNotification: Date,
+    games?: {
+      finished: number
+    }
+  };
+  authority: string;
+}
+
+export interface IUser extends IAbstractUser {
+  _id: string;
+}
